0f00d6b527809bb4f77ce6760e74c7edbcae6a38
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 19:37:44 +0200

    add standard endpoint "betriebsstellen"

--- src/main/java/CSV/RailwayPost/controller/RailwayPostController.java
+++ src/main/java/CSV/RailwayPost/controller/RailwayPostController.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
+@RequestMapping("betriebsstellen")
 public class RailwayPostController extends BaseController<RailwayPost> {
 }
