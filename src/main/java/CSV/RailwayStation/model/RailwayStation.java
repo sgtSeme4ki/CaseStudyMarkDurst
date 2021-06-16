8286ae1f7ae281d2be7da369286a88bc6696bdd3
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 19:30:33 +0200

    Add binding to respective CSV-property for mapping

--- src/main/java/CSV/RailwayStation/model/RailwayStation.java
+++ src/main/java/CSV/RailwayStation/model/RailwayStation.java
@@ -1,6 +1,7 @@
 package CSV.RailwayStation.model;
 
 import base.model.BaseEntity;
+import com.opencsv.bean.CsvBindByName;
 import lombok.*;
 
 import javax.persistence.Entity;
@@ -16,33 +17,50 @@ import javax.validation.constraints.NotNull;
 public class RailwayStation extends BaseEntity {
 
     @NotBlank
+    @CsvBindByName(column = "Abk")
     private String abbreviation;
 
     @NotBlank
+    @CsvBindByName(column = "Name")
     private String longName;
 
     @NotBlank
+    @CsvBindByName(column = "Kurzname")
     private String shortName;
 
     @NotBlank
+    @CsvBindByName(column = "Typ")
     private String type;
 
     @NotBlank
+    @CsvBindByName(column = "Betr-Zust")
     private String operatingCondition;
 
     @NotBlank
+    @CsvBindByName(column = "Primary Location code")
     private String primaryLocationCode;
 
     @NotBlank
+    @CsvBindByName(column = "UIC")
     private String RICS;
+
+    @CsvBindByName(column = "RB")
     private String RB;
 
     @NotBlank
+    @CsvBindByName(column = "gültig von")
     private String validFrom;
+
+    @CsvBindByName(column = "gültig bis")
     private String validUntil;
 
     @NotNull
+    @CsvBindByName(column = "Netz-Key")
     private Long netKey;
+
+    @CsvBindByName(column = "Fpl-rel")
     private Boolean scheduleRelevance;
+
+    @CsvBindByName(column = "Fpl-Gr")
     private Boolean scheduleEditingLimit;
 }
