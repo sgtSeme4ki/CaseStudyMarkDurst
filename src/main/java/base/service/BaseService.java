bfc0a03be79d0586c052c52b13cf1e6a86257054
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 17:54:34 +0200

    establish basic crud operation on BaseService

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -2,9 +2,15 @@ package base.service;
 
 import base.model.BaseEntity;
 import base.model.BaseRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -12,4 +18,42 @@ import org.springframework.stereotype.Service;
 public abstract class BaseService<T extends BaseEntity> {
 
     private final BaseRepository<T> repository;
+
+    @Transactional
+    public T create(@NonNull T t) {
+        T persisted = repository.save(t);
+        log.info("{} created with id {}", persisted.getClass().getSimpleName(), persisted.getId());
+
+        return persisted;
+    }
+
+    public T findById(@NonNull Long id) {
+        return repository.findById(id).orElseThrow(() -> {
+            throw new EntityNotFoundException("Entity with id " + id + " not found");
+        });
+    }
+
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+
+    @Transactional
+    public T update(@NonNull Long id, @NonNull T t) {
+        T update = findById(id);
+
+        BeanUtils.copyProperties(t, update);
+
+        T persisted = repository.save(t);
+
+        log.info("{} with id {} updated", persisted.getClass().getSimpleName(), persisted.getId());
+        return persisted;
+    }
+
+    @Transactional
+    public void delete(@NonNull Long id) {
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException("Entity with id " + id + " does not exist");
+        }
+        repository.deleteById(id);
+    }
 }
