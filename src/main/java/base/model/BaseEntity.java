b34e12e95a3177bdc961975e9b539a4bc41924f5
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 17:09:19 +0200

    Notate @MappedSuperclass to avoid creating a table of BaseEntity

--- src/main/java/base/model/BaseEntity.java
+++ src/main/java/base/model/BaseEntity.java
@@ -4,8 +4,11 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.MappedSuperclass;
+
 @Getter
 @Setter
 @EqualsAndHashCode
+@MappedSuperclass
 public class BaseEntity {
 }
