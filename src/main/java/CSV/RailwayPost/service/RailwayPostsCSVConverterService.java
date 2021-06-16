edd401fd7fba0679540c65050c1947d9d46dad8e
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 19:47:32 +0200

    make service inherit from base service

--- src/main/java/CSV/RailwayPost/service/RailwayPostsCSVConverterService.java
+++ src/main/java/CSV/RailwayPost/service/RailwayPostsCSVConverterService.java
@@ -1,4 +1,12 @@
 package CSV.RailwayPost.service;
 
-public class RailwayPostsCSVConverterService {
+import CSV.RailwayPost.model.RailwayPost;
+import base.model.BaseRepository;
+import base.service.BaseService;
+
+public class RailwayPostsCSVConverterService extends BaseService<RailwayPost> {
+
+    public RailwayPostsCSVConverterService(BaseRepository<RailwayPost> repository) {
+        super(repository);
+    }
 }
