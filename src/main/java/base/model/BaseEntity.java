c46f0c640decb35e231dcaa7e3d366d2240194bc
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 22:27:01 +0200

    Use @GeneratedValue(strategy = GenerationType.IDENTITY) for MySQL autoincrement for id

--- src/main/java/base/model/BaseEntity.java
+++ src/main/java/base/model/BaseEntity.java
@@ -4,6 +4,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
@@ -14,5 +16,6 @@ import javax.persistence.MappedSuperclass;
 public class BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
