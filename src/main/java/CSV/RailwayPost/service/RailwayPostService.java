ea2016afe4f33e4629acdb5d385a470c2af1f9d8
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 22:10:04 +0200

    Create RailwayPostService with deleteAll method

---
+++ src/main/java/CSV/RailwayPost/service/RailwayPostService.java
@@ -0,0 +1,24 @@
+package CSV.RailwayPost.service;
+
+import CSV.RailwayPost.model.RailwayPost;
+import CSV.RailwayPost.model.RailwayPostRepository;
+import base.service.BaseService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RailwayPostService extends BaseService<RailwayPost> {
+
+    private final RailwayPostRepository railwayPostRepository;
+
+    public RailwayPostService(RailwayPostRepository railwayPostRepository) {
+        super(railwayPostRepository);
+        this.railwayPostRepository = railwayPostRepository;
+    }
+
+    public void deleteAll() {
+        this.railwayPostRepository.deleteAll();
+        log.info("All Railway Posts deleted");
+    }
+}
