e83d880926f698827d675cad4e4274cc38ef1e49
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 17:36:36 +0200

    add constructor

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -1,8 +1,10 @@
 package base.service;
 
 import base.model.BaseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public abstract class BaseService<T extends BaseEntity> {
 }
