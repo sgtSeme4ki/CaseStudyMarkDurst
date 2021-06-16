79c333cb7725faa725525f2541614016e6c7746d
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 19:47:54 +0200

    add constructor

--- src/main/java/CSV/RailwayPost/controller/RailwayPostController.java
+++ src/main/java/CSV/RailwayPost/controller/RailwayPostController.java
@@ -1,6 +1,7 @@
 package CSV.RailwayPost.controller;
 
 import CSV.RailwayPost.model.RailwayPost;
+import CSV.RailwayPost.service.RailwayPostsCSVConverterService;
 import base.controller.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,4 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("betriebsstellen")
 public class RailwayPostController extends BaseController<RailwayPost> {
+
+    private final RailwayPostsCSVConverterService railwayPostsCSVConverterService;
+
+    public RailwayPostController(RailwayPostsCSVConverterService railwayPostsCSVConverterService) {
+        super(railwayPostsCSVConverterService);
+        this.railwayPostsCSVConverterService = railwayPostsCSVConverterService;
+    }
 }
