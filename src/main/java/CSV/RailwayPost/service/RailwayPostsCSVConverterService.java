ecf9b2b2861bc2b20003fa78969913ee720cc93f
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 22:03:04 +0200

    Inherit from BaseClass and use its methods

--- src/main/java/CSV/RailwayPost/service/RailwayPostsCSVConverterService.java
+++ src/main/java/CSV/RailwayPost/service/RailwayPostsCSVConverterService.java
@@ -2,23 +2,16 @@ package CSV.RailwayPost.service;
 
 import CSV.RailwayPost.model.RailwayPost;
 import CSV.RailwayPost.model.RailwayPostRepository;
-import base.service.BaseService;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
+import base.service.BaseCSVConverterService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
-public class RailwayPostsCSVConverterService extends BaseService<RailwayPost> {
+public class RailwayPostsCSVConverterService extends BaseCSVConverterService<RailwayPost> {
 
     private final RailwayPostRepository railwayPostRepository;
 
@@ -31,43 +24,10 @@ public class RailwayPostsCSVConverterService extends BaseService<RailwayPost> {
     }
 
     public void createRailwayPostsInMemory(MultipartFile file) {
-        checkIfPersisted();
-        this.transientRailwayPosts = convertCSVToBean(file);
+        createEntitiesInMemory(file);
     }
 
     public void createRailwayPostsInDatabase(MultipartFile file) {
-        checkIfPersisted();
-        this.transientRailwayPosts = convertCSVToBean(file);
-
-        this.transientRailwayPosts.forEach(railwayPost -> railwayPostRepository.save(railwayPost));
-        log.info("Railway posts successfully persisted");
-    }
-
-    public void deleteAll() {
-        railwayPostRepository.deleteAll();
-    }
-
-    private List<RailwayPost> convertCSVToBean(MultipartFile file) {
-        List<RailwayPost> railwayPosts = new ArrayList<>();
-
-        try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-            CsvToBean<RailwayPost> csvToBeanBuilder = new CsvToBeanBuilder(reader)
-                    .withType(RailwayPost.class)
-                    .build();
-
-            railwayPosts = csvToBeanBuilder.parse();
-            log.info("CSV-File successfully converted to RailwayPost-Bean");
-
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-        return railwayPosts;
-    }
-
-    private void checkIfPersisted() {
-        if (!super.findAll().isEmpty()) {
-            throw new RuntimeException("There are already railway posts in the database, consider deleting the old" +
-                    " ones first");
-        }
+        createEntitiesInDatabase(file);
     }
 }
