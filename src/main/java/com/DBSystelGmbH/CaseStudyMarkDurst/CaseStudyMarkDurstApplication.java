e3bf220c99a9335aae5c55d36ba9f2672e7337ee
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 16:22:19 +0200

    initial commit of spring initializr generated content

---
+++ src/main/java/com/DBSystelGmbH/CaseStudyMarkDurst/CaseStudyMarkDurstApplication.java
@@ -0,0 +1,13 @@
+package com.DBSystelGmbH.CaseStudyMarkDurst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyMarkDurstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyMarkDurstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DBSystelGmbH/CaseStudyMarkDurst/CaseStudyMarkDurstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBSystelGmbH.CaseStudyMarkDurst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyMarkDurstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
