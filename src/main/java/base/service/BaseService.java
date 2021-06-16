c9526bb2585100fbac8252f29086f3f4d142aa82
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 17:39:31 +0200

    Repository as field for access through service

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -1,10 +1,13 @@
 package base.service;
 
 import base.model.BaseEntity;
+import base.model.BaseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public abstract class BaseService<T extends BaseEntity> {
+
+    private final BaseRepository<T> repository;
 }
