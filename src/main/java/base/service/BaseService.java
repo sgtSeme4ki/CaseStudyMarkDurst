5967115b5b112a642b090e71479a15c71dd52c4d
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 17:42:15 +0200

    add @Slf4j for logging

--- src/main/java/base/service/BaseService.java
+++ src/main/java/base/service/BaseService.java
@@ -3,9 +3,11 @@ package base.service;
 import base.model.BaseEntity;
 import base.model.BaseRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public abstract class BaseService<T extends BaseEntity> {
 
