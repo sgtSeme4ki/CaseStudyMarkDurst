21d9fb73b5b79b421ee8a6d570b0a36bb8a59a40
sgtSeme4ki <markdurst98@gmail.com>
2021-06-16 18:16:37 +0200

    implement basic crud statements for controller

--- src/main/java/base/controller/BaseController.java
+++ src/main/java/base/controller/BaseController.java
@@ -1,7 +1,43 @@
 package base.controller;
 
+import base.model.BaseEntity;
+import base.service.BaseService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
-public abstract class BaseController {
+@Controller
+public abstract class BaseController<T extends BaseEntity> {
+
+    private final BaseService<T> baseService;
+
+    @PostMapping
+    public T create(@RequestBody @Valid T t) {
+        return baseService.create(t);
+    }
+
+    @GetMapping("{id}")
+    public T findById(@PathVariable("id") Long id) {
+        return baseService.findById(id);
+    }
+
+    @GetMapping
+    public List<T> findAll() {
+        return baseService.findAll();
+    }
+
+    @PutMapping("{id}")
+    public T update(@PathVariable("id") Long id, @RequestBody @NonNull @Valid T t) {
+        return baseService.update(id, t);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Long id) {
+        baseService.delete(id);
+    }
 }
